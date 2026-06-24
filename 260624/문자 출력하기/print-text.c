6c100065080bf7bb6d0914163f38ade481d43961
ssy0731 <shinseyoung0731@gmail.com>
2026-06-24 14:54:06 +0900

    [Passed] 문자 출력하기 (1ms, 0MB)

---
+++ 260624/문자 출력하기/print-text.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char a;
+    scanf("%c",&a);
+    int i;
+    for(i = 0; i < 8; i++){
+        printf("%c",a);
+    }
+    return 0;
+}
