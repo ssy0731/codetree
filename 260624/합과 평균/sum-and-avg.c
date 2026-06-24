3d5267549f0084d09769f28562a8c6c46e5a71bc
ssy0731 <shinseyoung0731@gmail.com>
2026-06-24 14:29:50 +0900

    [Passed] 합과 평균 (2ms, 0MB)

---
+++ 260624/합과 평균/sum-and-avg.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    scanf("%d%d",&a,&b);
+    double sum = a+b;
+    printf("%.0lf %.1lf",sum, sum/2);
+    return 0;
+}
