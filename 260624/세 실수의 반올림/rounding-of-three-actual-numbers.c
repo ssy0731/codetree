9047a02f0dfd223013af7bfd57b3d373c9dc5d08
ssy0731 <shinseyoung0731@gmail.com>
2026-06-24 14:24:29 +0900

    [Passed] 세 실수의 반올림 (1ms, 0MB)

---
+++ 260624/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    double a;
+    double b;
+    double c;
+    scanf("%lf",&a);
+    scanf("%lf",&b);
+    scanf("%lf",&c);
+    printf("%.3lf\n%.3lf\n%.3lf",a,b,c);
+    return 0;
+}
