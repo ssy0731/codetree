ecd5ba7408bfaf519ba3665c999cf6521d3d0a52
ssy0731 <shinseyoung0731@gmail.com>
2026-06-24 15:09:17 +0900

    [Passed] 점수 비교 (1ms, 0MB)

---
+++ 260624/점수 비교/score-comparison.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a, b, c, d;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    if (a>c && b>d)
+        printf("1");
+    else printf("0");
+    return 0;
+}
