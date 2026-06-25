92706686fd1a347de7e699fcad299ea2bf3e6f48
ssy0731 <shinseyoung0731@gmail.com>
2026-06-25 15:46:38 +0900

    [Passed] 2개의 정수를 서로 더하기 (1ms, 0MB)

---
+++ 260625/2개의 정수를 서로 더하기/add-two-integers-each-other.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    scanf("%d%d",&a,&b);
+    a += b;
+    b += a;
+    printf("%d %d",a,b);
+    return 0;
+}
