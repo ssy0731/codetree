22c0fe10373f03bf45e8cd0c51c632d1825359a9
ssy0731 <shinseyoung0731@gmail.com>
2026-06-25 17:56:08 +0900

    [Passed] 음수 구별하기 (2ms, 0MB)

---
+++ 260625/음수 구별하기/separate-negative-number.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    printf("%d\n",a);
+    if (a<0)
+        printf("minus");
+    return 0;
+}
