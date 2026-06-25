592233656f983257bbd9b8dbdb974fe59f31f72c
ssy0731 <shinseyoung0731@gmail.com>
2026-06-25 15:44:27 +0900

    [Passed] 입력받아 연산하기 (2ms, 1MB)

---
+++ 260625/입력받아 연산하기/take-input-and-operate.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    scanf("%d%d",&a,&b);
+    a += 87;
+    b %= 10;
+    printf("%d\n%d",a,b);
+    return 0;
+}
