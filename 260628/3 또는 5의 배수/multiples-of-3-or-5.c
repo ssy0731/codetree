fe401d7407b24d57fe2a8d8c856524ac04fe4b44
ssy0731 <shinseyoung0731@gmail.com>
2026-06-28 22:16:36 +0900

    [Passed] 3 또는 5의 배수 (1ms, 0MB)

---
+++ 260628/3 또는 5의 배수/multiples-of-3-or-5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    if (a%3==0) printf("YES\n");
+    else printf("NO\n");
+    if (a%5==0) printf("YES");
+    else printf("NO");
+    return 0;
+}
