9e6585acabd61ede2c9434844f0047a7176e436f
ssy0731 <shinseyoung0731@gmail.com>
2026-06-29 01:11:58 +0900

    [Passed] 중앙값 구하기 (1ms, 0MB)

---
+++ 260629/중앙값 구하기/find-the-median.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if (a<=b) {
+        if (c<=a) printf("%d",a);
+        else if (b<=c) printf("%d",b);
+        else printf("%d",c);
+    }
+    else {
+        if (c<=b) printf("%d",b);
+        else if (a<=c) printf("%d",a);
+        else printf("%d",c);
+    }
+    return 0;
+}
