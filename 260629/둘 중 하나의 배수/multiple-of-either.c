685c8f7fb1fde8467e8c274162dfdf3d80f206ac
ssy0731 <shinseyoung0731@gmail.com>
2026-06-29 00:31:58 +0900

    [Passed] 둘 중 하나의 배수 (1ms, 0MB)

---
+++ 260629/둘 중 하나의 배수/multiple-of-either.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    if (a%3==0 || a%5==0) printf("1");
+    else printf("0");
+    return 0;
+}
