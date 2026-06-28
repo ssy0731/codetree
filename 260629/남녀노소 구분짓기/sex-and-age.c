8a8b16b58c42b27b6fcf8aab54c676a00acdcc62
ssy0731 <shinseyoung0731@gmail.com>
2026-06-29 00:48:30 +0900

    [Passed] 남녀노소 구분짓기 (1ms, 0MB)

---
+++ 260629/남녀노소 구분짓기/sex-and-age.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    scanf("%d\n%d",&a,&b);
+    if (a==0) {
+        if (b>=19) printf("MAN");
+        else printf("BOY");
+    }
+    else {
+        if (b>=19) printf("WOMAN");
+        else printf("GIRL");
+    }
+    return 0;
+}
