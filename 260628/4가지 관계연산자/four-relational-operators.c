62727f85c8753268f71db5226a12c0756adcc330
ssy0731 <shinseyoung0731@gmail.com>
2026-06-28 22:19:14 +0900

    [Passed] 4가지 관계연산자 (2ms, 0MB)

---
+++ 260628/4가지 관계연산자/four-relational-operators.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d\n",a>=b);
+    printf("%d\n",a>b);
+    printf("%d\n",a<=b);
+    printf("%d",a<b);
+    return 0;
+}
