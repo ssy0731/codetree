345989850f8b3760bfeba5b00bf84c3dd82e7631
ssy0731 <shinseyoung0731@gmail.com>
2026-06-28 22:35:09 +0900

    [Passed] 세 수의 중간값 (1ms, 0MB)

---
+++ 260628/세 수의 중간값/median-of-three-numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if (b>a && b<c) printf("1");
+    else printf("0");
+    return 0;
+}
