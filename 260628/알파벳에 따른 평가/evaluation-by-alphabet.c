4e461ea98130ffdc8255ab2eba8df0f72edc5907
ssy0731 <shinseyoung0731@gmail.com>
2026-06-28 22:03:53 +0900

    [Passed] 알파벳에 따른 평가 (1ms, 0MB)

---
+++ 260628/알파벳에 따른 평가/evaluation-by-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char a;
+    scanf("%c",&a);
+    if (a=='S')
+        printf("Superior");
+    else if (a=='A')
+        printf("Excellent");
+    else if (a=='B')
+        printf("Good");
+    else if (a=='C')
+        printf("Usually");
+    else if (a=='D')
+        printf("Effort");
+    else
+        printf("Failure");
+    return 0;
+}
