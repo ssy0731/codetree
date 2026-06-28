d486eea44c80071d427f2827e03bd1f0ca31db68
ssy0731 <shinseyoung0731@gmail.com>
2026-06-28 22:31:27 +0900

    [Passed] 세 정수의 최솟값 (2ms, 0MB)

---
+++ 260628/세 정수의 최솟값/minimum-of-three-numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if (a<=b && a<=c) printf("%d",a);
+    else if (b<=a && b<=c) printf("%d",b);
+    else if (c<=a && c<=b) printf("%d",c);
+    return 0;
+}
