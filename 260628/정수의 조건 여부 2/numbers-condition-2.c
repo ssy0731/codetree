9b8fe10ee5fad5c25cbb88a41897b7911f6d202b
ssy0731 <shinseyoung0731@gmail.com>
2026-06-28 22:11:38 +0900

    [Passed] 정수의 조건 여부 2 (1ms, 0MB)

---
+++ 260628/정수의 조건 여부 2/numbers-condition-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    if (a==5)
+        printf("A");
+    if (a%2==0)
+        printf("B");
+    return 0;
+}
