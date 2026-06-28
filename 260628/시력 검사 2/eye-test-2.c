1e2858d7e32d3141f1b9c7ef4595564d188d6418
ssy0731 <shinseyoung0731@gmail.com>
2026-06-28 21:57:47 +0900

    [Passed] 시력 검사 2 (2ms, 0MB)

---
+++ 260628/시력 검사 2/eye-test-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    double a;
+    scanf("%lf",&a);
+    if (a>=1.0)
+        printf("High");
+    else if (a>=0.5)
+        printf("Middle");
+    else
+        printf("Low");
+    return 0;
+}
