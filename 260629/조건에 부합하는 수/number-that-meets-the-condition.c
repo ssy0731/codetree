bffbc3729941b6c4545dfc272729e8949c6c11bc
ssy0731 <shinseyoung0731@gmail.com>
2026-06-29 18:26:37 +0900

    [Passed] 조건에 부합하는 수 (2ms, 0MB)

---
+++ 260629/조건에 부합하는 수/number-that-meets-the-condition.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    for (int i=1;i<=a;i++) {
+        if (i%2==0 && i%4!=0) continue;
+        else if (i/8%2==0) continue;
+        else if (i%7<4) continue;
+        else printf("%d ",i);
+    }
+    return 0;
+}
