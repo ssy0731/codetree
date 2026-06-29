2ae5d9585b90fc6474de6d2bc3f167ea4a5183af
ssy0731 <shinseyoung0731@gmail.com>
2026-06-29 16:09:46 +0900

    [Passed] 별 N개 출력하기 (2ms, 0MB)

---
+++ 260629/별 N개 출력하기/print-n-stars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    int i = 0;
+    while (i<a) {
+        printf("*\n");
+        i++;
+    }
+    return 0;
+}
