e2fceb340f2d5bbefce6c80c25d7d50b47f66cb7
ssy0731 <shinseyoung0731@gmail.com>
2026-06-29 16:20:57 +0900

    [Passed] 문자열 출력하기 (2ms, 0MB)

---
+++ 260629/문자열 출력하기/print-string.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    for (int i = 1;i<=a;i++)
+        printf("LeebrosCode\n");
+    return 0;
+}
