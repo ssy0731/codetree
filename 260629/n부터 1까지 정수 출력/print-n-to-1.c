af4b30429d9c40e59b90766ebdf93ca011dd092e
ssy0731 <shinseyoung0731@gmail.com>
2026-06-29 16:16:13 +0900

    [Passed] n부터 1까지 정수 출력 (1ms, 0MB)

---
+++ 260629/n부터 1까지 정수 출력/print-n-to-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    while (a>=1) {
+        printf("%d ",a);
+        a--;
+    }
+    return 0;
+}
