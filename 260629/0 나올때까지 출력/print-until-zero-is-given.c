a08f318fd111c3f0dd8518bc03b1e180877f1cad
ssy0731 <shinseyoung0731@gmail.com>
2026-06-29 18:37:25 +0900

    [Passed] 0 나올때까지 출력 (2ms, 0MB)

---
+++ 260629/0 나올때까지 출력/print-until-zero-is-given.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    while(1) {
+        int a;
+        scanf("%d",&a);
+        if (a==0) break;
+        printf("%d\n",a);
+    }
+    return 0;
+}
