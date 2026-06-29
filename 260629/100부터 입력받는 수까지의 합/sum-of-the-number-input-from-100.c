8f94ce9a6699f9a7372fb156742938a2204e83d0
ssy0731 <shinseyoung0731@gmail.com>
2026-06-29 17:10:44 +0900

    [Passed] 100부터 입력받는 수까지의 합 (1ms, 0MB)

---
+++ 260629/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int s = 0;
+    int a;
+    scanf("%d",&a);
+    for (a; a<=100;a++) s+=a;
+    printf("%d",s);
+    return 0;
+}
