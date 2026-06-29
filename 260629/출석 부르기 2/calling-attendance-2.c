fa2773dd5b72d81fda40702bfacd483ad7a2c568
ssy0731 <shinseyoung0731@gmail.com>
2026-06-29 18:41:20 +0900

    [Passed] 출석 부르기 2 (2ms, 0MB)

---
+++ 260629/출석 부르기 2/calling-attendance-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    while(1) {
+        int a;
+        scanf("%d",&a);
+        if (a>4) {
+            printf("Vacancy");
+            break;
+        }
+        else if (a==1) printf("John\n");
+        else if (a==2) printf("Tom\n");
+        else if (a==3) printf("Paul\n");
+        else if (a==4) printf("Sam\n");
+    }
+    return 0;
+}
