18c9a581895c03d922890aaab821d12ce2042e9f
ssy0731 <shinseyoung0731@gmail.com>
2026-06-29 16:36:59 +0900

    [Passed] 자연수면 출력 (1ms, 0MB)

---
+++ 260629/자연수면 출력/output-only-natural-number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if (a>0) {
+        for (int i=1; i<=b; i++)
+            printf("%d",a);
+    }
+    else
+        printf("0");
+    return 0;
+}
