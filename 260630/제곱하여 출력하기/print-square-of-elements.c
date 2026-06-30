91222888e0d667c2f39569feaae1cf83db2edea7
ssy0731 <shinseyoung0731@gmail.com>
2026-06-30 23:51:13 +0900

    [Passed] 제곱하여 출력하기 (1ms, 0MB)

---
+++ 260630/제곱하여 출력하기/print-square-of-elements.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+    for (int i=0; i<n; i++) {
+        int a;
+        scanf("%d ",&a);
+        printf("%d ",a*a);
+    }
+    return 0;
+}
