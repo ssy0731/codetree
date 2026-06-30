a384d30c1ac3e29163fd07cabd89edc133bee1cf
ssy0731 <shinseyoung0731@gmail.com>
2026-06-30 21:35:32 +0900

    [Passed] 규칙을 찾아 출력하기 (1ms, 0MB)

---
+++ 260630/규칙을 찾아 출력하기/find-a-rule-and-print.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            if (i==0 || j==0 || i==n-1 || j==n-1) {
+                printf("* ");
+            }
+            else if(i>j) {
+                printf("* ");
+            }
+            else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
