f6a2e4142576509c3c94580032a6e8f1b21befd2
ssy0731 <shinseyoung0731@gmail.com>
2026-06-30 22:49:05 +0900

    [Passed] 알파벳 출력하기 2 (1ms, 0MB)

---
+++ 260630/알파벳 출력하기 2/print-alphabet-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+    char a = 'A';
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            if (i<=j){
+                printf("%c ",a);
+                a++;
+                if (a>'Z') a='A';
+            }
+            else printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
