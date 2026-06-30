5fa1f2ef9e1944c974113bd0f1343f22510a8e42
ssy0731 <shinseyoung0731@gmail.com>
2026-06-30 22:28:19 +0900

    [Passed] 숫자 피라미드 (2ms, 0MB)

---
+++ 260630/숫자 피라미드/number-pyramid.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            if (j<=i) printf("%d ",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
