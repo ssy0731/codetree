e38501a05baa29a125f6b59970e6d4484dbbc841
ssy0731 <shinseyoung0731@gmail.com>
2026-06-30 22:34:11 +0900

    [Passed] 특정 규칙에 따른 숫자 출력 (2ms, 0MB)

---
+++ 260630/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+    int c=1;
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            if (j>=i) {
+                printf("%d ",c);
+                c++;
+                if (c>9) c=1;
+            }
+            else printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
