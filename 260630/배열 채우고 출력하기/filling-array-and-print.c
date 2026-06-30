a3bd1842464fd403908f2076c9d514bf344be0be
ssy0731 <shinseyoung0731@gmail.com>
2026-06-30 23:10:24 +0900

    [Passed] 배열 채우고 출력하기 (1ms, 0MB)

---
+++ 260630/배열 채우고 출력하기/filling-array-and-print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char arr[10];
+    for (int i=0;i<10;i++){
+        scanf("%c ",&arr[i]);
+    }
+    for (int i=0;i<10;i++){
+        printf("%c",arr[10-i-1]);
+    }
+    return 0;
+}
